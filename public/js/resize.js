f2296b1dd44ef3b1f4d87f0641a82037fa25dd29
Attila Miklosi <attila.miklosi@gmail.com>
2011-07-20 11:33:57 +0100

    a

--- public/js/resize.js
+++ public/js/resize.js
@@ -1,13 +1,12 @@
 this.writeToAndResizeIfNeeded = function (buffer, maxWidth, maxHeight, outFilename, successCallback, errorCallback) {
-	var binary = buffer.toString('binary');
-	im.identify({data:binary}, function(err, features) {
+	im.identify({data:buffer.toString('binary')}, function(err, features) {
 		if (err) {
 			console.log(err);
 			res.send(JSON.stringify({success: false}));
 		}
 		if (features.width > maxWidth || features.height > maxHeight) {
 			console.log('Image too big, resizing');
-			im.resize({srcData: binary, dstPath: outFilename, width: 800, format: 'jpg'}, function(err) {
+			im.resize({srcData: buffer.toString('binary'), dstPath: outFilename, width: 800, format: 'jpg'}, function(err) {
 				if (err) {
 					console.log(err);
 					errorCallback();
