5478bf0998bf98e3596731d54553afc6e26849ae
Attila Miklosi <attila.miklosi@gmail.com>
2011-07-22 14:34:48 +0100

    a

--- flickr/flickrGateway.js
+++ flickr/flickrGateway.js
@@ -6,12 +6,14 @@ this.getRandomFlickrUrl = function(tags, successCallback, errorCallback) {
 	tags = tags.replace(/[^a-zA-Z0-9,]/g, '');
 	console.log("Searching FlickR for ", tags);
 	flickr.photos.search({tags:tags, tag_mode: 'all', media: 'photos', per_page: 1}, function(error, results) {
+		console.log("Response 1 ",error);
 		var randomPage = Math.floor(Math.random() * Number(results.pages));
 		if (error) {
 			errorCallback(error);
 			return;
 		}
 		flickr.photos.search({tags:tags, tag_mode: 'all', media: 'photos', extras: 'url_z,url_l,url_o', per_page: 10, page: Math.floor(randomPage / 10)}, function(error, results) {
+			console.log("Response 2 ",error);
 			if (error) {
 				errorCallback(error);
 				return;
@@ -22,6 +24,8 @@ this.getRandomFlickrUrl = function(tags, successCallback, errorCallback) {
 			if (photo) {
 				var url = photo.url_z;
 				successCallback(url);
+			} else {
+				errorCallback("Image not found");
 			}
 		});
 	});
