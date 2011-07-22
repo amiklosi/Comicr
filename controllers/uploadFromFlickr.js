db9ec18c32337e0ede095b1208510639ecf52ea2
Attila Miklosi <attila.miklosi@gmail.com>
2011-07-22 12:23:13 +0100

    flickr

---
+++ controllers/uploadFromFlickr.js
@@ -0,0 +1,7 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: mikl
+ * Date: 22/07/2011
+ * Time: 11:53
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ globals.js
@@ -0,0 +1,46 @@
+this.uploadFromUrl = function(url, req, mainRes) {
+	var matches = /^.*?:\/\/(.*?)(\/.*)$/.exec(url);
+	var options = {
+		host: matches[1],
+		port: 80,
+		path: matches[2]
+	}
+	var str = Hash.md5(new Date().toString() + req.sessionID);
+	var request = http.get(options, function(res) {
+		res.setEncoding('binary')
+		var buffers = [];
+		var sumLength = 0;
+		res.on("data", function(chunk) {
+			buffers.push(new Buffer(chunk, 'binary'));
+			sumLength += chunk.length;
+		});
+		res.on('end', function(chunk) {
+			console.log("Total Length: ", sumLength);
+			var pos = 0;
+			var bigBuffer = new Buffer(sumLength + 1)
+			for (var i = 0; i < buffers.length; i++) {
+				buffers[i].copy(bigBuffer, pos, 0);
+				pos += buffers[i].length;
+			}
+			req.session.iUrl = str;
+
+			resize.writeToAndResizeIfNeeded(bigBuffer, 800, 600, mainDirname + '/public/upload/' + str,
+					function() {
+						mainRes.writeHead(200, {'content-type': 'text/json' });
+						mainRes.write(JSON.stringify({success: true, id: str}));
+						mainRes.end('\n');
+					},
+					function() {
+						mainRes.writeHead(200, {'content-type': 'text/json' });
+						mainRes.write(JSON.stringify({success: false}));
+						mainRes.end('\n');
+					}
+			);
+		})
+	});
+	request.addListener('error', function(){
+		mainRes.writeHead(200, {'content-type': 'text/json' });
+		mainRes.write(JSON.stringify({success: false}));
+		mainRes.end('\n');
+	});
+}
